e7ec6ad64bb9c825aa9d1e5d202a372f7b03b117
token factory pol <rahmatpourmba@gmail.com>
2026-07-24 02:18:09 +0330

    fix: remove rainbowkit

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,29 +1,13 @@
 ﻿import './index.css';
-import '@rainbow-me/rainbowkit/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { LanguageProvider } from './i18n.jsx';
 import App from './App.jsx';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import { polygon } from 'wagmi/chains';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-
-const config = getDefaultConfig({
-  appName: 'Token Factory',
-  projectId: '3a8170812b534d0ff9d794f19a901d84',
-  chains: [polygon],
-  ssr: false,
-});
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>,
 );
